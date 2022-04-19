959adc83b778b55415283a4819b80f2d75c69f34
vlada <vladasabelnikova@gmail.com>
2022-04-19 19:58:02 +0300

    Убрали рога
    Всё.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Привет, мир!"
+    return "Привет, всем!"
 
 
 if __name__ == '__main__':
-    app.run(port=5001, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5001))
+    serve(app, port=port, host='0.0.0.0')
