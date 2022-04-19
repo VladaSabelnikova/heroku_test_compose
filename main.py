699f247b099d4c9be2ae2d41c1d68c1a35a3a7d0
vlada <vladasabelnikova@gmail.com>
2022-04-19 17:12:55 +0300

    Готово
    ОЧень подробный коммит )))

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Привет, мир!"
+
+
+if __name__ == '__main__':
+    app.run(port=5001, host='0.0.0.0')
